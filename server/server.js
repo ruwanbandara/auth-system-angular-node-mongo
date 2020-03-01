ff8021c4696e3bf20e7b05f208d8cce0bb418363
Ruwan Bandara <ruwan.se.uok@gmail.com>
2020-03-01 23:36:15 +0530

    post request

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+var cors = require('cors')
+var mongoose = require('mongoose')
 
 const PORT = 3000
 const api = require('./routes/api')
 const app = express()
 app.use(bodyParser.json())
+
+app.use(cors());
 
 app.use('/api', api)
 
