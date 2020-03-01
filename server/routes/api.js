07d96f89e356b2cae44c9d1825bb2d6a689b5097
Ruwan Bandara <ruwan.se.uok@gmail.com>
2020-03-02 00:47:56 +0530

    user login post rquest

--- server/routes/api.js
+++ server/routes/api.js
@@ -29,6 +29,25 @@ router.post('/register', (req, res) =>{
         }
     })
 })
+router.post('/login',(req, res) =>{
+    let userData = req.body
+
+    User.findOne({email: userData.email}, (error, user) =>{
+        if (error){
+            console.log(error)
+        }else {
+            if (!user){
+                res.status(401).send('Invalid email')
+            }else{
+                if (user.password !== userData.password){
+                    res.status(401).send('Invalid Password')
+                }else {
+                    res.status(200).send(user)
+                }
+            }
+        }
+    })
+})
 
 module.exports = router
 
