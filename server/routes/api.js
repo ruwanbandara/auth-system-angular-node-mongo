ea0a121a40b3afc4e007d124f329df52ed6e4098
Ruwan Bandara <ruwan.se.uok@gmail.com>
2020-03-02 07:42:56 +0530

    add event hardcode

--- server/routes/api.js
+++ server/routes/api.js
@@ -48,6 +48,47 @@ router.post('/login',(req, res) =>{
         }
     })
 })
+router.get('/events', (req, res) =>{
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        },
+        {
+            "_id": "6",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        }
+
+    ]
+})
 
 module.exports = router
 
