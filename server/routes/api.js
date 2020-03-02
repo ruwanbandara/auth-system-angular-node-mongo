6b294eddbedf5e72ab601aa26c9c71140ccbb3de
Ruwan Bandara <ruwan.se.uok@gmail.com>
2020-03-02 08:00:12 +0530

    add componnet (login, register, events, special )

--- server/routes/api.js
+++ server/routes/api.js
@@ -88,6 +88,49 @@ router.get('/events', (req, res) =>{
         }
 
     ]
+    res.json(events)
+})
+router.get('/special', (req, res) =>{
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        },
+        {
+            "_id": "6",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2020-04-10-23T18:25:43.511Z"
+        }
+
+    ]
+    res.json(events)
 })
 
 module.exports = router
