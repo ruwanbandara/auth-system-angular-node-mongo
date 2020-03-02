fc28c5b873264f4c68207872e389511bfca50744
Ruwan Bandara <ruwan.se.uok@gmail.com>
2020-03-02 10:30:13 +0530

    router for events

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {EventsComponent} from "./events/events.component";
-import {SpecialEventsComponent} from "./special-events/special-events.component";
-import {LoginComponent} from "./login/login.component";
-import {RegisterComponent} from "./register/register.component";
+import {EventsComponent} from './events/events.component';
+import {SpecialEventsComponent} from './special-events/special-events.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/events',
+    pathMatch: 'full'
+
+  },
   {
     path: 'events',
     component: EventsComponent
