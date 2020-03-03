348f9ca51f27340126c238249b3cc2640e07394c
Ruwan Bandara <ruwan.se.uok@gmail.com>
2020-03-03 06:42:15 +0530

    final commit

--- frontEnd/src/app/auth.service.ts
+++ frontEnd/src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import { Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class AuthService {
   // tslint:disable-next-line:variable-name
   private _loginUrl = 'http://localhost:3000/api/login';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _router: Router) { }
 
   registerUser(user) {
     return this.http.post<any>(this._registerUrl, user);
@@ -19,5 +20,15 @@ export class AuthService {
 
   loginUser(user) {
     return this.http.post<any>(this._loginUrl, user);
+  }
+  loggedIn() {
+    return !!localStorage.getItem('token');
+  }
+  logoutUser(){
+    localStorage.removeItem('token');
+    this._router.navigate(['/events'])
+  }
+  getToken(){
+    return localStorage.getItem('token')
   }
 }
