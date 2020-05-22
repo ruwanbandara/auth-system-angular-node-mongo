44166e21cf555262eb56a6dbd01802cc7be87e9f
Ruwan Bandara <ruwan.se.uok@gmail.com>
2020-05-22 22:46:56 +0530

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const router = express.Router()
-const User = require('../models/user')
-const mongoose = require('mongoose')
-const db = "mongodb://localhost/eventsdb"
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const User = require('../models/user');
+const mongoose = require('mongoose');
+const db = "mongodb://localhost/eventsdb";
 
 
 
@@ -15,40 +15,40 @@ mongoose.connect(db, err =>{
         console.log('Connected to mongodb')
 
     }
-})
+});
 function verifyToken(req, res, next){
     if (!req.headers.authorization){
         return res.status(401).send('Unauthorized request')
     }
-    let token = req.headers.authorization.split(' ')[1]
+    let token = req.headers.authorization.split(' ')[1];
     if (token === 'null'){
         return res.status(401).send('Unauthorized request')
     }
-    let payload = jwt.verify(token, 'secretKey')
+    let payload = jwt.verify(token, 'secretKey');
     if(!payload){
         return res.status(401).send('Unauthorized request')
     }
-    req.userId = payload.subject
+    req.userId = payload.subject;
     next()
 }
 router.get('/',( req, res) => {
     res.send('From API route')
-})
+});
 router.post('/register', (req, res) =>{
-    let userData =req.body
-    let user = new User(userData)
+    let userData =req.body;
+    let user = new User(userData);
     user.save((error, registeredUser)=>{
         if (error){
             console.log(error)
         }else {
-            let payload = { subject: registeredUser.id}
-            let token = jwt.sign(payload, 'secretKey')
+            let payload = { subject: registeredUser.id};
+            let token = jwt.sign(payload, 'secretKey');
             res.status(200).send({token})
         }
     })
-})
+});
 router.post('/login',(req, res) =>{
-    let userData = req.body
+    let userData = req.body;
 
     User.findOne({email: userData.email}, (error, user) =>{
         if (error){
@@ -60,14 +60,14 @@ router.post('/login',(req, res) =>{
                 if (user.password !== userData.password){
                     res.status(401).send('Invalid Password')
                 }else {
-                    let payload = {subject: user.id}
-                    let token= jwt.sign(payload, 'secretKey')
+                    let payload = {subject: user.id};
+                    let token= jwt.sign(payload, 'secretKey');
                     res.status(200).send({token})
                 }
             }
         }
     })
-})
+});
 router.get('/events', (req, res) =>{
     let events = [
         {
@@ -107,9 +107,9 @@ router.get('/events', (req, res) =>{
             "date": "2020-04-10"
         }
 
-    ]
+    ];
     res.json(events)
-})
+});
 router.get('/special', verifyToken, (req, res) =>{
     let events = [
         {
@@ -149,9 +149,9 @@ router.get('/special', verifyToken, (req, res) =>{
             "date": "2020-04-10"
         }
 
-    ]
+    ];
     res.json(events)
-})
+});
 
-module.exports = router
+module.exports = router;
 
